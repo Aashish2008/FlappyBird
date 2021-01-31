268259e842d88a69612147bbcc350e4968affff0
Aashish <71557064+Aashish2008@users.noreply.github.com>
2021-01-31 13:29:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,67 +1,163 @@
-var flappy,flappyimg,flappydown,flappydownimg;
+var Bird, Bird_Falling, Bird_Rising, BirdIMG, Bird_FallingIMG, Bird_RisingIMG;
 
-var bg,bgimg;
+var pipeup,pipeupIMG, pipedown, pipedownIMG,pipeupgroup, pipedowngroup;
 
-var pipeup,pipeup1,pipeup2,pipeup3;
+var Bg, BgIMG;
 
-var pipedown,pipedown1,pipedown2,pipedown3;
+var Ground;
 
-var gameover,gameoverimg;
+var GameOver, Restart, GameOverIMG, ResetIMG;
 
-var restart,restartimg;
+var Start, StartIMG;
 
-var start,startimg;
+var logo, logoIMG;
 
-var play,playimg;
+var lives; 
+
+var gameState=PLAY;
+//var SERVE=0;
+var PLAY=1;
+var END=0;
+var score= 0;
 
 function preload(){
 
-  //flappyimg
-  flappyimg = loadAnimation("images/bird1.png","images/bird2.png");
-
-  //flappydownimg
-  flappydownimg = loadImage("images/flappydownimg");
-
-  //bgimg
-  bgimg = loadImage("images/background.png");
-
-  //pipeup1
-  pipeup1 = loadImage("images/pipeup1.png");
-
-  //pipeup2
-  pipeup2 = loadImage("images/pipeup2.png");
-
-  //pipeup3
-  pipeup3 = loadImage("images/pipeup3.png");
-
-  //pipedown1
-  pipeudown1 = loadImage("images/pipedown1.png");
-
-  //pipedown2
-  pipeudown2 = loadImage("images/pipedown2.png");
-
-  //pipedown3
-  pipeudown3 = loadImage("images/pipedown3.png");
-
-  //gameoverimg
-  gameoverimg = loadImage("images/gameover.png");
-
-  //restartimg
-  restartimg = loadImage("images/restartimg.jpg");
-
-  //startimg
-  startimg = loadImage("images/start.jpg");
-
-  //playimg
-  playimg = loadImage("images/play.png")
+BirdIMG= loadAnimation("flappybird1.png","flappybird2.png");
+//Bird_FallingIMG= loadImage("Images/Falling_Down.png");
+BgIMG= loadImage("background.png");
+GameOverIMG= loadImage("gameover.png");
+ResetIMG= loadImage("reset.png");
+pipeupIMG= loadImage("pipeup.png");
+pipedownIMG= loadImage("pipedown.png");
+StartIMG= loadImage("start.png");
+logoIMG= loadImage("logo.png");
 }
 
-function setup() {
-  createCanvas(1200,400);
-  createSprite(400, 200, 50, 50);
+function setup(){
+createCanvas(1530,720);
+    
+Bg=createSprite(740,350,1530,720);
+Bg.addImage(BgIMG);
+Bg.x = Bg.width/2;
+Bg.velocityX=-6;
+
+Bird= createSprite(730,310,20,20);
+Bird.addAnimation("Bird",BirdIMG);
+Bird.scale= 0.15;
+
+Ground=createSprite(500,675,1200,25);
+Ground.visible= false;
+Ground.x=Ground.width/2;
+Ground.velocityX=-6;
+
+Restart= createSprite(675,430);
+Restart.addImage(ResetIMG);
+Restart.scale=0.3;
+
+Start= createSprite(750,400);
+Start.addImage(StartIMG);
+Start.scale= 0.3;
+
+GameOver= createSprite(675,340);
+GameOver.addImage(GameOverIMG);
+GameOver.scale= 0.3;
+
+logo= createSprite(740,200,100,100);
+logo.addImage(logoIMG);
+
+GameOver.visible=false;
+Restart.visible=false;
+
+pipeupgroup=new Group();
+pipedowngroup=new Group();
+
+fill("black");  
+score=0;
+
+}
+function draw(){
+//background("lightblue");
+drawSprites();
+textSize(20);
+text("SCORE: "+score,1350,50);
+
+if(mousePressedOver(Start)){
+    Bird.velocityY = -12 ;
+    Start.visible=false;
+    logo.visible=false;
+    gameState=PLAY;
+            }
+if(Bg.x<0){
+    Bg.x=Bg.width/2;
+    }
+
+if(gameState===PLAY){
+score = score+Math.round(getFrameRate()/30);  
+
+Bg.velocityX=-4;
+    
+if(keyDown("space")){
+   Bird.velocityY = -12 ;
+   }
+   Bird.velocityY=Bird.velocityY + 0.8;
+   Bird.velocityX=0;
+
+
+UpPipes();
+DownPipes();
+
+if(Bird.isTouching(pipeupgroup) || Bird.isTouching(Ground)||Bird.isTouching(pipedowngroup)){
+   gameState=END;
+    }
+
+}   
+else if(gameState=== END){
+   GameOver.visible=true;
+   Restart.visible=true;
+   Bird.velocityY=0;
+   Bg.velocityX=0;
+   Ground.velocityX=0;
+   Bird.y=300;
+    Bird.x=300;
+pipeupgroup.setVelocityXEach(0);
+  pipedowngroup.setVelocityXEach(0);
+}
+if(mousePressedOver(Restart)) {
+    reset();  
+  }
+ 
+
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+function reset(){
+    gameState=PLAY;
+    GameOver.visible=false;
+    Restart.visible=false;
+    pipeupgroup.destroyEach();
+    pipedowngroup.destroyEach();
+    Bird.y=300;
+    Bird.x=300;
+    score = 0;
+  }
+
+function UpPipes(){
+    if(frameCount%75===0){
+    pipeup=createSprite(1500,250,40,100);    
+    pipeup.y=Math.round(random(0,50));
+    pipeup.addImage(pipedownIMG);
+    pipeup.velocityX=-4;
+    pipeup.scale=0.6;
+    pipeupgroup.add(pipeup);
+    }
+}
+
+function DownPipes(){
+    if(frameCount%75===0){
+    pipedown=createSprite(1500,500,40,100);
+    pipedown.y=Math.round(random(600,740));
+    pipedown.addImage(pipeupIMG);
+    pipedown.velocityX=-4;
+    pipedown.scale=0.6;
+    pipedowngroup.add(pipedown);
+    }
 }
